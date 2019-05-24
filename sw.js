1bac8160778eb00cd82261741432bf7db2a4477e
Raymond Luong <rayluong92@gmail.com>
2019-05-23 22:58:37 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,35 +26,35 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5578863b82bb2a0d2aa0.js"
+    "url": "webpack-runtime-dfaf483d08d1a5ba6707.js"
   },
   {
-    "url": "app-6bd96006dbd216bbc724.js"
+    "url": "app-09fb850b1beb93dedc08.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-4cccf54412dab7855d79.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5eee1076e0f15b5cb3103189bc9349d0"
+    "revision": "011b0d8902f1bebd3678b2b5984a2c8f"
   },
   {
-    "url": "styles.2f88c74ec7d209b4b22e.css"
+    "url": "styles.9ef198bc23b3d4453ea2.css"
   },
   {
-    "url": "component---src-pages-404-jsx-f990161849a33924f06a.js"
+    "url": "component---src-pages-404-jsx-f885fd4b10685341c3c0.js"
   },
   {
-    "url": "2-f3c3fea6ebf885d738aa.js"
+    "url": "2-c7745c75b781c854389c.js"
   },
   {
     "url": "1-0572df43eff0f210c925.js"
   },
   {
-    "url": "styles-4c63715691e32ab51009.js"
+    "url": "styles-ba9961f32dca1be70126.js"
   },
   {
-    "url": "static/d/340/path---404-html-516-62a-lj2UoE6S3hy8NUbxkz8kMjp1ucY.json"
+    "url": "static/d/794/path---404-html-516-62a-LHTeMbVoKhXi8iF3tbwwKSiX1Q.json"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
