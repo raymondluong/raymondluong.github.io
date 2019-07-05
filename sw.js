5519782aee63837d36b09e6a4d14606a3afe2f8b
Raymond Luong <rayluong92@gmail.com>
2019-07-05 12:52:05 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9a4229b89b540ba5cc58.js"
+    "url": "webpack-runtime-b1479d815250be9c4400.js"
   },
   {
     "url": "app-4333fd8e319641669657.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6f7d28ae90c14caeba93290288f01e2f"
+    "revision": "a35ee70d9c9697a6bbe7d72e43bfe0b7"
   },
   {
     "url": "styles.2f88c74ec7d209b4b22e.css"
