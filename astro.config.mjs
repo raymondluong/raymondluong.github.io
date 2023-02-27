cb793e392ca6bd04762db6cfd8bc7c842d5ec9c7
Raymond Luong <raymond@gem.com>
2023-02-26 18:48:37 -0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.raymondluong.com/',
+  site: 'https://raymondluong.github.io',
   integrations: [mdx(), sitemap(), tailwind()],
   base: '/',
   build: {
